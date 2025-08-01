a26831449e9d2543150b4117220216f1ccfdedbe
void166 <rekihhhrenchin@gmail.com>
2025-08-02 00:48:11 +0800

    save data and generate html for payment summary

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,4 +1,6 @@
-export function formatCurrency(priceCents){
-    return (priceCents/100).toFixed(2);
-}
-export default formatCurrency;
+export function formatCurrency(priceCents) {
+    return (Math.round(priceCents) / 100).toFixed(2);
+  }
+  
+  export default formatCurrency;
+  
