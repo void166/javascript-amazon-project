bba763f54f8f9a298bf1ea2c97e2fa3090c4148b
void166 <rekihhhrenchin@gmail.com>
2025-08-02 00:07:15 +0800

    save data and generate html for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,55 +1,53 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
-if(!cart){
-    cart = [
-        {
-          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-          quantity: 2,
-        },
-        {
-          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-          quantity: 1,
-        },
-      ];
+
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId: '1'
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryOptionId: '2'
+  }];
 }
 
-
-function saveToStorage(){
-    localStorage.setItem('cart',JSON.stringify(cart));
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 export function addToCart(productId) {
   let matchingItem;
+
   cart.forEach((cartItem) => {
     if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
   });
 
-  const quantitySelector = document.querySelector(
-    `.js-quantity-selector-${productId}`
-  );
-  const quantity = Number(quantitySelector.value);
-
   if (matchingItem) {
-    matchingItem.quantity += quantity;
+    matchingItem.quantity += 1;
   } else {
     cart.push({
-      productId,
-      quantity,
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: '1'
     });
   }
 
   saveToStorage();
 }
 
-export function removeFromCart(productId){
-    const newCart = [];
+export function removeFromCart(productId) {
+  const newCart = [];
 
-    cart.forEach((cartItem)=>{
-        if(cartItem.productId !== productId){
-            newCart.push(cartItem);
-        }
-    });
-    cart = newCart;
-    saveToStorage();
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
 }
