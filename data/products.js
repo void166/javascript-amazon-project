e07fd0ec6664df3e34afce3fb6801b3f0cd17f92
void166 <rekihhhrenchin@gmail.com>
2025-08-09 11:30:29 +0800

    use inheritance and polymorphism

--- data/products.js
+++ data/products.js
@@ -25,14 +25,33 @@ class Product{
     this.rating = productDetails.rating;
     this.priceCents = productDetails.priceCents;
   }
-
   getStarsUrl(){
     return `images/ratings/rating-${this.rating.stars * 10}.png`;
   }
   getPrice(){
     return `$${formatCurrency(this.priceCents)}`;
   }
+  extraInfoHtml(){
+    return '';
+  }
 }
+class Clothing extends Product{
+  sizeChartLink;
+  
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink= productDetails.sizeChartLink;
+  }
+
+  extraInfoHtml(){
+    //super.extraInfoHtml();
+    return `
+      <a href="${this.sizeChartLink}" target="_blank
+      ">Size Chart</a>
+    `;
+  }
+}
+
 
 export const products = [
   {
@@ -694,6 +713,8 @@ export const products = [
     ]
   }
 ].map((productDetails)=>{
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
-console.log(products)
